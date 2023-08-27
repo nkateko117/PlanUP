f839de2ece6680e943c792a2b856e5cdb20fa038
Nkateko Maluleke <102928817+nkateko117@users.noreply.github.com>
2023-08-28 01:28:22 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PlanUP_App',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
