a955684a570daf89042bf34ccaea4448c580e91c
Nkateko Maluleke <u19075716@up.ac.za>
2023-09-08 01:33:17 +0200

    API updates

--- PlanUP/Models/StudentModule.cs
+++ PlanUP/Models/StudentModule.cs
@@ -1,6 +1,13 @@
-﻿namespace PlanUP.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlanUP.Models
 {
     public class StudentModule
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ModuleID { get; set; }
+        public string ModuleName { get; set; }
     }
 }
