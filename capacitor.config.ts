88af865d10da3a59607841a0c1c3fb302588aee7
Nkateko Maluleke <u19075716@up.ac.za>
2023-09-24 21:45:07 +0200

    ionic cap open android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PlanUP_App',
+  appId: 'com.nkateko117.PlanUP',
+  appName: 'PlanUP App',
   webDir: 'www',
   server: {
     androidScheme: 'https'
